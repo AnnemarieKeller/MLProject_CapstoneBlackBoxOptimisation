01f212da9ada9d7626b8052d0efdf5b96312d039
Annemarie <solvingproblemsallthetime@gmail.com>
2025-11-22 17:06:32 +0000

    fixed indent

--- scripts/utils/utils.py
+++ scripts/utils/utils.py
@@ -4,5 +4,5 @@ def set_alpha(y_train):
     """Automatically determine alpha from y scale
     """
 # Automatically determine alpha from y scale
-y_range = np.max(y_train) - np.min(y_train)
-alpha = max(1e-6, 1e-4 * y_range)  # small fraction of y range
+    y_range = np.max(y_train) - np.min(y_train)
+    alpha = max(1e-6, 1e-4 * y_range)  # small fraction of y range
